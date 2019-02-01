fb6c0aaaa5a35c3098100b4a5e0bf10491bc0c82
ronaldo3r <ronaldo3-0827@hotmail.com>
2019-01-31 20:28:01 -0500

    Mas avanzado

--- app/src/main/java/appmoviles/com/clase2/MainActivity.java
+++ app/src/main/java/appmoviles/com/clase2/MainActivity.java
@@ -1,13 +1,53 @@
 package appmoviles.com.clase2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_reg = findViewById(R.id.tv_reg);
+        tv_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Register.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("SALIR")
+                .setMessage("¿Desea Salir?")
+                .setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        }).setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        MainActivity.super.onBackPressed();
+                        //System.exit(0);
+                    }
+                });
+
+        builder.show();
     }
 }
