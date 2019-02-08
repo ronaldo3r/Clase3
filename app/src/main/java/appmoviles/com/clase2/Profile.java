5f00e345d891fa86fc61c4b2d73fe3c23dadb6b9
ronaldo3r <ronaldo3-0827@hotmail.com>
2019-02-07 21:03:52 -0500

    final clase 3

--- app/src/main/java/appmoviles/com/clase2/Profile.java
+++ app/src/main/java/appmoviles/com/clase2/Profile.java
@@ -14,6 +14,8 @@ public class Profile extends AppCompatActivity {
     private RelativeLayout root_profile;
     private Button btn_salir_profile;
 
+    //Implementar el metodo de abajo en funciones landa
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
